0877f24293a90fa90ba8b3bdcdda592c7b949cc7
Eiichi Takamura <takamura.corefine@gmail.com>
2026-05-20 12:16:00 +0900

    銀座シックスメンバ追加

--- local-static-server.cjs
+++ local-static-server.cjs
@@ -9,6 +9,9 @@ const types = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".webp": "image/webp",
 };
 
 http
